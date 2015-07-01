9121427ed43fbe8f65cf0eaf7d9777a5752970c6
黄佳峰 <huangjiafeng@beedeiMac.local>
2015-07-01 09:49:25 +0800

    add new TestGit class

---
+++ Demo_Git_Local/TestGit.h
@@ -0,0 +1,14 @@
+//
+//  TestGit.h
+//  Demo_Git_Local
+//
+//  Created by 黄佳峰 on 15/7/1.
+//  Copyright (c) 2015年 tarenaaaa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestGit : NSObject
+-(void)testFunction;
+
+@end
---
+++ Demo_Git_Local/TestGit.m
@@ -0,0 +1,18 @@
+//
+//  TestGit.m
+//  Demo_Git_Local
+//
+//  Created by 黄佳峰 on 15/7/1.
+//  Copyright (c) 2015年 tarenaaaa. All rights reserved.
+//
+
+#import "TestGit.h"
+
+@implementation TestGit
+
+
+-(void)testFunction{
+    NSLog(@"TestGit");
+    
+}
+@end
