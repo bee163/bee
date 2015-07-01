b9bd5338ed202b176e43708a48f8b4cde666dfc4
黄佳峰 <huangjiafeng@beedeiMac.local>
2015-07-01 09:32:37 +0800

    Initial Commit

---
+++ Demo_Git_Local/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo_Git_Local
+//
+//  Created by 黄佳峰 on 15/7/1.
+//  Copyright (c) 2015年 tarenaaaa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo_Git_Local/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo_Git_Local
+//
+//  Created by 黄佳峰 on 15/7/1.
+//  Copyright (c) 2015年 tarenaaaa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
